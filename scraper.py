4d2a8982c1e526e701b9ab091e8990be1a2b2681
Danielle Carlos <dkcarlos@uci.edu>
2023-10-20 10:29:26 -0700

    Scraper.py - Fleshed out extract_next_links
    Edited to implement comments and created list to store hyperlinks that will be returned.

--- scraper.py
+++ scraper.py
@@ -7,15 +7,20 @@ def scraper(url, resp):
 
 def extract_next_links(url, resp):
     # Implementation required.
+    next_links = []
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
     # resp.error: when status is not 200, you can check the error here, if needed.
+    if(resp.status == 200):
+        pass
+    else:
+        print(resp.error)
     # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    return next_links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
